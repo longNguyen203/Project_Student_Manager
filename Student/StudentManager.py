ca5bd6b6e7a67fd6255d119a829eef11dab4b04c
LongDE2003 <longlovers2003@gmail.com>
2023-11-10 22:03:29 +0700

    Update

---
+++ Student/StudentManager.py
@@ -0,0 +1,68 @@
+from Student.Student import Student
+
+
+class StudentManager:
+    
+    list_Student: list[Student] = []
+    
+    def frameField(self) -> str:
+        print("{:<8}{:<25}{:<9}{:<6}{:<25}{:<10}".format("ID", "Ten", "NamSinh", "Khoa", "Major", "GPA"))
+    
+    def addStudent(self) -> None:
+        
+        print("*** Them Sinh Vien ***")
+        
+        id = input("-->ID: ")
+        ten = input("-->Ten: ")
+        namSinh = int(input("-->NamSinh: "))
+        khoa = int(input("-->Khoa: "))
+        major = input("-->Major: ")
+        GPA = float(input("-->GPA: "))
+        
+        StudentManager.list_Student.append(Student(id, ten, namSinh, khoa, major, GPA))
+        
+    def printStudent(self) -> None:
+        self.frameField()
+        for student in StudentManager.list_Student:
+            print(student.showProfile())
+            
+    def deleteStudent(self) -> None:
+        id = input("-->ID: ")
+        for student in StudentManager.list_Student:
+            if id == student.getId():
+                StudentManager.list_Student.remove(student)
+                print("*** Xoa Thanh Cong ***")
+                return 
+        print("*** Khong Ton Tai Sinh Vien Nay ***")
+        
+    def editStudent(self) -> None:
+        id = input("-->ID: ")
+        for student in StudentManager.list_Student:
+            if id == student.getId():
+                try:
+                    student.setTen(input("-->Sua Ten: "))
+                    student.setNamSinh(int(input("-->Sua NamSinh: ")))
+                    student.setKhoa(int(input("-->Sua Khoa: ")))
+                    student.setMajor(input("-->Sua Major: "))
+                    student.setGPA(float(input("-->Sua GPA: ")))
+                    print("*** Sua Thanh Cong ***")
+                     
+                except ValueError:
+                    print("Loi! Dau vao khong hop le!!!!")
+                else:
+                    return
+        print("*** Khong Ton Tai Sinh Vien Nay ***")
+        
+    def findStudent(self) -> None:
+        id = input("-->ID: ")
+        for student in StudentManager.list_Student:
+            if id == student.getId():
+                self.frameField()
+                student.showProfile()
+                return 
+        print("*** Khong Ton Tai Sinh Vien Nay ***")
+                
+                
+            
+
+    
--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-from Student.Students import Students
+from Student.StudentManager import StudentManager
+import os, sys
 
+def Menu():
+    print("1. Them Sinh vien")
+    print("2. Xem Danh sach Sinh vien")
+    print("3. Xoa Sinh Vien")
+    print("4. Sua Sinh Vien")
+    print("5. Tim Sinh Vien")
+    print("0. Return Home")
 
 def main() -> None:
-    student = Students()
-    student.addStudent()
-    
-def __
+    student = StudentManager()
+    Menu()
+    while True:
+        select = int(input("Choose select: "))
+        os.system('cls')
+        if select == 1:
+            student.addStudent()
+        elif select == 2:
+            student.printStudent()
+        elif select == 3:
+            student.deleteStudent()
+        elif select == 4:
+            student.editStudent()
+        elif select == 5:
+            student.findStudent()
+        elif select == 0:
+            Menu()
+        else:
+            print("Thoat Thành Cong")
+            break
+
+if __name__ == "__main__":
+    main()
